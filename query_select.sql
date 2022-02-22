a308e8c463044162014bb6675bdeeba2ad608075
Zanco1993 <zanconatofabio@gmail.com>
2022-02-22 15:30:48 +0100

    es complete

--- query_select.sql
+++ query_select.sql
@@ -13,7 +13,7 @@ WHERE `date_of_birth` BETWEEN '1990-01-01' AND '1990-12-31';
 -------------------------------------------------------------------------------------------------------------------
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-SELECT `id`
+SELECT `id`, `name`
 FROM `courses`
 WHERE `cfu` > '10';
 
@@ -26,5 +26,40 @@ WHERE YEAR(CURRENT_DATE) - YEAR(`date_of_birth`) > 30;
 
 -------------------------------------------------------------------------------------------------------------------
 
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
+-- laurea (286)
+SELECT `id`, `period`, `year`
+FROM `courses`
+WHERE `period` = 'I semestre' 
+	AND `year` = 1;
 
+-------------------------------------------------------------------------------------------------------------------
 
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
+-- 20/06/2020 (21)
+
+-- per chiarimento, in questa query terrò conto anche degli esami che sono iniziati dalle 14 in poi quindi:
+SELECT `id`, `hour`, `date`
+FROM `exams`
+WHERE `date` = '2020-06-20'
+	AND HOUR(`hour`) >= 14;
+
+-------------------------------------------------------------------------------------------------------------------
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT `id`, `name`, `level`
+FROM `degrees`
+WHERE `level` = 'magistrale';
+
+-------------------------------------------------------------------------------------------------------------------
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(`id`) AS num_departments
+FROM `departments`;
+
+-------------------------------------------------------------------------------------------------------------------
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT `id`, `name`, `surname`
+FROM `teachers`
+WHERE `phone` IS NULL;
