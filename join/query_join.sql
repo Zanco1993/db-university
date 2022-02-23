d5ffb0a4eb6bf225a5f52d961358a66578c00d1f
Zanco1993 <zanconatofabio@gmail.com>
2022-02-23 16:24:27 +0100

    prime 5 query completate in JOIN

---
+++ join/query_join.sql
@@ -0,0 +1,43 @@
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT `students`.`id`, `students`.`name`, `degrees`.`name`
+FROM `students`
+JOIN `degrees`
+	ON `students`.`degree_id` = `degrees`.`id`
+WHERE `degrees`.`name` = "Corso di laurea in Economia";
+
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `departments`.id, `departments`.name,`degrees`.`name`
+FROM `departments`
+JOIN `degrees`
+	ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` LIKE "%neuroscienze";
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `course_teacher`.`teacher_id`,`courses`.`name`
+FROM `courses`
+JOIN `course_teacher`
+	ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `course_teacher`.`teacher_id` = 44;
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
+--    relativo dipartimento, in ordine alfabetico per cognome e nome
+
+-- ho eseguito un doppio join perchè volevo ottenere anche l'informazione del nome dipartimento e non solo il codice
+-- che si sarebbe trovata nella tabella ponte
+SELECT `students`.`id`, `students`.`name`, `students`.`surname`, `degrees`.`name` AS `nome_corso`, `departments`.`name` AS `nome_dipartimento` 
+FROM `departments`
+JOIN `degrees`
+	ON `departments`.`id` = `degrees`.`department_id`
+JOIN `students`
+	ON `degrees`.`id` = `students`.`degree_id`
+ORDER BY `students`.`name`, `students`.`surname`;
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT `degrees`.`id` AS `corso_di_laurea_id`, `degrees`.`name`, `course_teacher`.`course_id`, `course_teacher`.`teacher_id`
+FROM `degrees`
+JOIN `courses`
+	ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+	ON `course_teacher`.`course_id` = `courses`.`id`;
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
