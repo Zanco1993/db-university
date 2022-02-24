463cbb862c1930c7a54eb6ffdba82c1591dc4f5f
Zanco1993 <zanconatofabio@gmail.com>
2022-02-24 12:06:25 +0100

    BONUS fix

--- join/query_join.sql
+++ join/query_join.sql
@@ -6,7 +6,7 @@ JOIN `degrees`
 WHERE `degrees`.`name` = "Corso di laurea in Economia";
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-SELECT `departments`.id, `departments`.name,`degrees`.`name`
+SELECT `departments`.`id`, `departments`.name,`degrees`.`name`
 FROM `departments`
 JOIN `degrees`
 	ON `departments`.`id` = `degrees`.`department_id`
@@ -52,11 +52,11 @@ ORDER BY `course_teacher`.`teacher_id` ASC;
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
 --    superare ciascuno dei suoi esami
 
-SELECT `students`.`id` AS `codice_studente`, `exams`.`course_id` AS `codice_materia`, COUNT(`exams`.`id`) AS `numero_tentativi_appello`
+SELECT `students`.`id` AS `codice_studente`, `exams`.`course_id` AS `codice_materia`, COUNT(`exams`.`id`) AS `numero_tentativi_appello`, MAX(`exam_student`.`vote`) AS `voto_massimo`
 FROM `students`
 INNER JOIN `exam_student`
 	ON `students`.`id` = `exam_student`.`student_id`
 INNER JOIN `exams`
 	ON `exam_student`.`exam_id` = `exams`.`id`
-GROUP BY `students`.`id`, `exams`.`course_id`;
-
+GROUP BY `students`.`id`, `exams`.`course_id`
+	HAVING `voto_massimo` >= 18;
